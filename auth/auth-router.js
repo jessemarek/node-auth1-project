daa6896554673b2c894433c19af23cb6ddb3edf5
unknown <dethgrenade@gmail.com>
2020-06-15 14:34:43 -0700

    users endpoint requires you to be logged in to access and is conrolled by a session

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,7 +24,7 @@ router.post('/login', (req, res) => {
     Users.findBy({ username })
         .then(([user]) => {
             if (user && bcrypt.compareSync(password, user.password)) {
-                //req.session.user = { id: user.id, username: user.username }
+                req.session.user = { id: user.id, username: user.username }
                 res.status(200).json({ message: `${username} is logged in` })
             }
             else {
